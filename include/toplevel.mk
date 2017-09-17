adff1b3b0b995b6e8a2d7feb4403fb4be279ae28
Arturo Rinaldi <arty.net2@gmail.com>
2017-09-18 01:14:33 +0200

    rpi3 : adding first main config file

--- include/toplevel.mk
+++ include/toplevel.mk
@@ -115,6 +115,11 @@ defconfig: scripts/config/conf prepare-tmpinfo FORCE
 	@if [ ! -s .config -a -e $(HOME)/.openwrt/defconfig ]; then cp $(HOME)/.openwrt/defconfig .config; fi
 	$< --defconfig=.config Config.in
 
+defrpi2: scripts/config/conf prepare-tmpinfo FORCE
+	touch .config
+	@if [ ! -s .config -a -e $(HOME)/configfiles/armconfig ]; then cp $(HOME)/configfiles/armconfig .config; fi
+	$< --defconfig=.config Config.in
+
 confdefault-y=allyes
 confdefault-m=allmod
 confdefault-n=allno
