202bb2cc6835fc7991071e8258c0b33ee1074606
Arturo Rinaldi <arty.net2@gmail.com>
2017-09-21 03:15:11 +0200

    shellinabox : updating security certificate entries

--- package/linino-arm/shellinabox/files/gen-cert.sh
+++ package/linino-arm/shellinabox/files/gen-cert.sh
@@ -5,7 +5,7 @@ cd /tmp
 
 # creating certificate.pem file
 openssl genrsa -out server.key 1024
-(echo IT; echo Italy; echo Strambino; echo Arduino Srl; echo SoftwareEngineering; echo Arduino Srl; echo info@arduino.org; echo .; echo .) | openssl req -new -key server.key -out server.csr
+(echo IT; echo Italy; echo Messina; echo SmartMe.io; echo SoftwareEngineering; echo SmartMe.io; echo info@arduino.org; echo .; echo .) | openssl req -new -key server.key -out server.csr
 cp server.key server.key.org
 openssl rsa -in server.key.org -out server.key
 openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
