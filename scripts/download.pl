fdc1acdf9eae9456122c45cade601e7dbef6dec4
Arturo Rinaldi <arty.net2@gmail.com>
2017-11-09 15:12:51 +0100

    download.pl : adding buildroot source repository as additional mirror

--- scripts/download.pl
+++ scripts/download.pl
@@ -259,6 +259,7 @@ foreach my $mirror (@ARGV) {
 push @mirrors, 'http://sources.lede-project.org';
 push @mirrors, 'http://mirror2.openwrt.org/sources';
 push @mirrors, 'http://downloads.openwrt.org/sources';
+push @mirrors, 'http://sources.buildroot.net';
 
 while (!-f "$target/$filename") {
 	my $mirror = shift @mirrors;
