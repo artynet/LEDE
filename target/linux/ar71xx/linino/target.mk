995d39960e22ba6a60bad542dfe46e170c847867
Arturo Rinaldi <arty.net2@gmail.com>
2018-01-31 03:10:58 +0100

    ar71xx : cleaning up legacy build for Linino devices

--- target/linux/ar71xx/linino/target.mk
+++ target/linux/ar71xx/linino/target.mk
@@ -1,8 +1,6 @@
-BOARDNAME:=Linino
+BOARDNAME:=Linino devices with NOR flash
 FEATURES += squashfs
 
 define Target/Description
 	Build firmware images for generic Atheros AR71xx/AR933x LININO boards.
 endef
-
-
